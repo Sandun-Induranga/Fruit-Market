ebb77212424888716c9cf95eada277c1c8bbc9a2
Sandun Induranga <sandutharu40@gmail.com>
2023-03-26 13:13:44 +0530

    Header Component Completed

--- Frontend/frontend/src/components/Header/Header.tsx
+++ Frontend/frontend/src/components/Header/Header.tsx
@@ -1,13 +1,14 @@
 import { Component } from "react";
+import logo from "../../assets/logo.png";
 
 export default class Header extends Component {
   render() {
     return (
-      <div className="w-full h-32 border flex items-center p-6">
-        <section className="w-1/2 h-full bg-slate-600 flex items-center">
-          <h1>LOGO</h1>
+      <div className="w-full h-28 flex items-center p-6">
+        <section className="w-1/2 h-full flex items-center">
+          <img src={logo} alt="" />
         </section>
-        <section className="w-1/2 h-full text-xl bg-slate-200 flex justify-end gap-10 items-center">
+        <section className="w-1/2 h-full text-xl text-green-900 flex justify-end gap-10 items-center">
           <a href="#">Home</a>
           <a href="#">About</a>
           <a href="#">Products</a>
