8d186eb2c219a9acfb10fff80099eb26d1e32e6e
Sandun Induranga <sandutharu40@gmail.com>
2023-03-26 13:08:14 +0530

    Header Component Updated

--- Frontend/frontend/src/App.tsx
+++ Frontend/frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-green-800 text-6xl">React</h1>
+      <Header />
     </div>
   );
 }
--- Frontend/frontend/src/components/Header/Header.tsx
+++ Frontend/frontend/src/components/Header/Header.tsx
@@ -1,7 +1,19 @@
-import React, { Component } from "react";
+import { Component } from "react";
 
 export default class Header extends Component {
   render() {
-    return <div>Header</div>;
+    return (
+      <div className="w-full h-32 border flex items-center p-6">
+        <section className="w-1/2 h-full bg-slate-600 flex items-center">
+          <h1>LOGO</h1>
+        </section>
+        <section className="w-1/2 h-full text-xl bg-slate-200 flex justify-end gap-10 items-center">
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">Products</a>
+          <a href="#">Contact</a>
+        </section>
+      </div>
+    );
   }
 }
