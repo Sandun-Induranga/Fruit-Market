c4b3a6db0c3de3f8b6c8a7fb08602aafc9acc104
Sandun Induranga <sandutharu40@gmail.com>
2023-03-26 19:42:16 +0530

    Home Component Updated

--- Frontend/frontend/src/pages/Home/Home.tsx
+++ Frontend/frontend/src/pages/Home/Home.tsx
@@ -4,26 +4,26 @@ import homeImage from "../../assets/home-image.png";
 export default class Home extends Component {
   render() {
     return (
-      <div className="min-h-screen flex pt-28">
+      <div className="h-screen flex pt-28">
         <aside className="p-6 h-full flex">
-          <section className="w-full h-full flex flex-col justify-center items-center">
+          <section className="w-full h-full flex flex-col mt-36 items-center">
             <div className="w-1/2 flex flex-col">
-              <h1 className="text-7xl text-green-800 mb-10">Fresh Fruits</h1>
+              <h1 className="text-7xl text-green-800 mb-20">Fresh Fruits</h1>
               <p className="text-2xl text-green-600">
                 Lorem ipsum dolor sit amet consectetur adipisicing
                 elit.molestiae quas vel sint commodi repudiandae
               </p>
             </div>
-            <div className="mt-10 text-xl w-1/2 flex gap-12">
-              <button className="bg-green-800 w-28 py-2 rounded-lg text-white">
+            <div className="mt-16 text-xl w-1/2 flex gap-12">
+              <button className="bg-green-800 border-2 border-green-800 w-28 py-2 rounded-lg text-white hover:bg-white hover:text-green-800">
                 Login
               </button>
-              <button className="bg-white border-2 border-green-800 w-28 py-2 rounded-lg text-green-800">
+              <button className="bg-white border-2 border-green-800 w-28 py-2 rounded-lg text-green-800 hover:bg-green-800 hover:text-white">
                 Sign Up
               </button>
             </div>
           </section>
-          <section className="w-1/2 h-full flex justify-center items-end">
+          <section className="w-1/2 h-full flex justify-center items-end p-6">
             <img src={homeImage} alt="" />
           </section>
         </aside>
