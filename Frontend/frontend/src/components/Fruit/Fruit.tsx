e4135f13df6bf1570f6517b1e1d28956e5e37118
Sandun Induranga <sandutharu40@gmail.com>
2023-03-26 22:33:45 +0530

    Component Fruit Updated

--- Frontend/frontend/src/components/Fruit/Fruit.tsx
+++ Frontend/frontend/src/components/Fruit/Fruit.tsx
@@ -8,6 +8,9 @@ export default class Fruit extends Component {
         <h2 className="text-xl text-green-900">Pineapple</h2>
         <img src={fruit} className="mt-6 w-1/2" />
         <p className="text-green-500 text-2xl mt-6">150.00 LKR</p>
+        <button className="mt-4 text-green-700 bg-green-50 py-2 w-full rounded hover:text-green-50 hover:bg-green-700">
+          Add to cart
+        </button>
       </div>
     );
   }
