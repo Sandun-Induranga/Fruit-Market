9ecb52707f516b061bfc99186b40b96344a3bafb
Sandun Induranga <sandutharu40@gmail.com>
2023-03-27 14:49:59 +0530

    AppHeader Component Updated

--- Frontend/frontend/src/components/AppHeader/AppHeader.tsx
+++ Frontend/frontend/src/components/AppHeader/AppHeader.tsx
@@ -8,7 +8,7 @@ export default class AppHeader extends Component {
         <section className="w-1/2 h-full flex items-center">
           <img src={logo} alt="" />
         </section>
-        <section className="w-1/2 h-full text-xl text-green-800 flex justify-end gap-10 items-center">
+        <section className="w-1/2 h-full text-xl text-green-50 flex justify-end gap-10 items-center">
           <a href="#">Home</a>
           <a href="#">About</a>
           <a href="#">Products</a>
