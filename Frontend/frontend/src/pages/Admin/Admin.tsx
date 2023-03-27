29943a6b16d7e17452fa1398f73648bf7b881ef3
Sandun Induranga <sandutharu40@gmail.com>
2023-03-27 16:05:10 +0530

    Dashboard Component Updated

--- Frontend/frontend/src/pages/Admin/Admin.tsx
+++ Frontend/frontend/src/pages/Admin/Admin.tsx
@@ -1,13 +1,11 @@
 import { Component } from "react";
-import AdminMenu from "../../components/AdminMenu";
-import AppHeader from "../../components/AppHeader";
+import Dashboard from "./Dashboard";
 
 export default class Admin extends Component {
   render() {
     return (
       <div className="bg-green-50">
-        <AppHeader />
-        <AdminMenu />
+        <Dashboard />
       </div>
     );
   }
--- Frontend/frontend/src/pages/Admin/Dashboard/Dashboard.tsx
+++ Frontend/frontend/src/pages/Admin/Dashboard/Dashboard.tsx
@@ -1,7 +1,14 @@
 import { Component } from "react";
+import AdminMenu from "../../../components/AdminMenu";
+import AppHeader from "../../../components/AppHeader";
 
 export default class Dashboard extends Component {
   render() {
-    return <div>Dashboard</div>;
+    return (
+      <div className="min-h-screen w-full pt-20 pl-1/6 bg-black">
+        <AppHeader />
+        <AdminMenu />
+      </div>
+    );
   }
 }
