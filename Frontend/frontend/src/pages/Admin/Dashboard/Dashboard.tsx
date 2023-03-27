e53efe7821c0f696d538fa4d9645799221723f7e
Sandun Induranga <sandutharu40@gmail.com>
2023-03-27 21:11:26 +0530

    Created Frontend

--- Frontend/frontend/src/pages/Admin/Dashboard/Dashboard.tsx
+++ Frontend/frontend/src/pages/Admin/Dashboard/Dashboard.tsx
@@ -5,7 +5,7 @@ import AppHeader from "../../../components/AppHeader";
 export default class Dashboard extends Component {
   render() {
     return (
-      <div className="min-h-screen w-full pt-20 pl-1/6 bg-black">
+      <div className="min-h-screen w-full pt-20 pl-1/6 bg-green-50">
         <AppHeader />
         <AdminMenu />
       </div>
