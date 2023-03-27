59c6a7eec73c8eee733118e16c01fa465b38ab3c
Sandun Induranga <sandutharu40@gmail.com>
2023-03-27 14:56:51 +0530

    AppHeader Component Updated

--- Frontend/frontend/src/components/AppHeader/AppHeader.tsx
+++ Frontend/frontend/src/components/AppHeader/AppHeader.tsx
@@ -1,15 +1,20 @@
 import { Component } from "react";
 import logo from "../../assets/logo.png";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 export default class AppHeader extends Component {
   render() {
     return (
-      <div className="w-full h-28 flex items-center p-6 fixed top-0 bg-green-800">
+      <div className="w-full h-20 flex items-center p-6 fixed top-0 bg-green-800">
         <section className="w-1/2 h-full flex items-center">
-          <img src={logo} alt="" />
+          <img src={logo} className="h-full" alt="" />
         </section>
         <section className="w-1/2 h-full text-xl text-green-50 flex justify-end gap-10 items-center">
-          <a href="#">Logout</a>
+          <a href="#">
+            <p className="flex items-center">
+              <LogoutIcon /> Logout
+            </p>
+          </a>
         </section>
       </div>
     );
