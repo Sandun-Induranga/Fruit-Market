734b0931033cd55311ade48be0b754e15a0c7b30
Sandun Induranga <sandutharu40@gmail.com>
2023-03-27 14:48:54 +0530

    AppHeader Component Updated

--- Frontend/frontend/src/components/AppHeader/AppHeader.tsx
+++ Frontend/frontend/src/components/AppHeader/AppHeader.tsx
@@ -1,7 +1,20 @@
 import { Component } from "react";
+import logo from "../../assets/logo.png";
 
 export default class AppHeader extends Component {
   render() {
-    return <div>AppHeader</div>;
+    return (
+      <div className="w-full h-28 flex items-center p-6 fixed top-0 bg-green-800">
+        <section className="w-1/2 h-full flex items-center">
+          <img src={logo} alt="" />
+        </section>
+        <section className="w-1/2 h-full text-xl text-green-800 flex justify-end gap-10 items-center">
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">Products</a>
+          <a href="#">Contact</a>
+        </section>
+      </div>
+    );
   }
 }
