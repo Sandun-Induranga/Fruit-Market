984e2057f779fc5fa68aaf331b8788a836450744
Sandun Induranga <sandutharu40@gmail.com>
2023-03-27 15:35:03 +0530

    AdminMenu Component Updated

--- Frontend/frontend/src/components/AdminMenu/AdminMenu.tsx
+++ Frontend/frontend/src/components/AdminMenu/AdminMenu.tsx
@@ -2,6 +2,15 @@ import { Component } from "react";
 
 export default class AdminMenu extends Component {
   render() {
-    return <div className="h-screen bg-white w-1/6 shadow-lg">AdminMenu</div>;
+    return (
+      <div className="h-screen bg-white w-1/6 shadow-lg pt-20">
+        <nav>
+          <a href="#">Home</a>
+          <a href="#">Products</a>
+          <a href="#">Orders</a>
+          <a href="#">Reports</a>
+        </nav>
+      </div>
+    );
   }
 }
