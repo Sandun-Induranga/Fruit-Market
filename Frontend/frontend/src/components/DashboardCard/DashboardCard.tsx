5c51400c00689b2a508c4a96a6fcd4b52d11da4d
Sandun Induranga <sandutharu40@gmail.com>
2023-03-28 00:54:39 +0530

    Component DashboardCard Updated

--- Frontend/frontend/src/components/DashboardCard/DashboardCard.tsx
+++ Frontend/frontend/src/components/DashboardCard/DashboardCard.tsx
@@ -1,9 +1,15 @@
 import { Component } from "react";
+import LocalAtmIcon from "@mui/icons-material/LocalAtm";
 
 export default class DashboardCard extends Component {
   render() {
     return (
-      <div className="">
+      <div className="w-full h-48 bg-white p-6 rounded-lg">
+        <span className="text-6xl text-green-700">
+          <h4>
+            <LocalAtmIcon fontSize="inherit" />
+          </h4>
+        </span>
         <h1>Total Orders</h1>
       </div>
     );
--- Frontend/frontend/src/pages/Admin/Dashboard/Dashboard.tsx
+++ Frontend/frontend/src/pages/Admin/Dashboard/Dashboard.tsx
@@ -1,15 +1,15 @@
 import { Component } from "react";
-import AdminMenu from "../../../components/AdminMenu";
-import AppHeader from "../../../components/AppHeader";
 import DashboardCard from "../../../components/DashboardCard";
 
 export default class Dashboard extends Component {
   render() {
     return (
-      <div className="min-h-screen w-full pt-24 pl-64 bg-green-50">
-        {/* <AppHeader />
-        <AdminMenu /> */}
-        <DashboardCard />
+      <div className="min-h-screen w-full pt-24 pl-64 pr-4 bg-green-50">
+        <section className="grid grid-cols-3 gap-4">
+          <DashboardCard />
+          <DashboardCard />
+          <DashboardCard />
+        </section>
       </div>
     );
   }
