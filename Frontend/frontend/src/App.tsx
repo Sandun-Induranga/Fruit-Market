ef8fc0a4e3409310abef35285700bd224633c847
Sandun Induranga <sandutharu40@gmail.com>
2023-03-27 13:38:48 +0530

    Updated Contact Component

--- Frontend/frontend/src/App.tsx
+++ Frontend/frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Products from "./pages/Products";
+import Contact from "./pages/Contact";
 
 export default class App extends Component {
   render() {
@@ -12,6 +13,7 @@ export default class App extends Component {
         <Home />
         <About />
         <Products />
+        <Contact />
       </div>
     );
   }
--- Frontend/frontend/src/pages/Contact/Contact.tsx
+++ Frontend/frontend/src/pages/Contact/Contact.tsx
@@ -1,7 +1,41 @@
+import { Button, TextField } from "@mui/material";
 import { Component } from "react";
 
 export default class Contact extends Component {
   render() {
-    return <div>Contact</div>;
+    return (
+      <div className="mt-20 flex flex-col items-center">
+        <h1 className="text-7xl text-green-800 text-center">Contact Us</h1>
+        <form className="w-2/3 flex flex-col gap-10 mt-20 mb-20" action="">
+          <TextField
+            label="Email"
+            type="email"
+            color="success"
+            fullWidth={true}
+            required
+          />
+          <TextField
+            label="Subject"
+            type="text"
+            color="success"
+            fullWidth={true}
+            required
+          />
+          <TextField
+            label="Message"
+            type="textl"
+            color="success"
+            variant="outlined"
+            multiline
+            minRows={5}
+            placeholder="Your Message"
+            required
+          />
+          <Button color="success" variant="contained">
+            Message
+          </Button>
+        </form>
+      </div>
+    );
   }
 }
