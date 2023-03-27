19499f73f9f0e02e24769fc06b36c5f152959f3b
Sandun Induranga <sandutharu40@gmail.com>
2023-03-27 15:37:55 +0530

    AdminMenu Component Updated

--- Frontend/frontend/src/components/AdminMenu/AdminMenu.tsx
+++ Frontend/frontend/src/components/AdminMenu/AdminMenu.tsx
@@ -4,7 +4,7 @@ export default class AdminMenu extends Component {
   render() {
     return (
       <div className="h-screen bg-white w-1/6 shadow-lg pt-20">
-        <nav className="flex flex-col py-20 pl-10 gap-8">
+        <nav className="flex flex-col py-20 pl-10 gap-8 text-green-800 text-lg">
           <a href="#">Home</a>
           <a href="#">Products</a>
           <a href="#">Orders</a>
