096b8b6587797cd006744e2b4f816a4cd98d84c8
Sandun Induranga <sandutharu40@gmail.com>
2023-03-28 20:57:46 +0530

    Customer Controller Updated

--- Backend/src/main/java/com/fresh/fruit_market/controller/CustomerController.java
+++ Backend/src/main/java/com/fresh/fruit_market/controller/CustomerController.java
@@ -1,4 +1,18 @@
 package com.fresh.fruit_market.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customer")
+@CrossOrigin
 public class CustomerController {
+
+    @GetMapping
+    public String test(){
+        return "test";
+    }
+
 }
