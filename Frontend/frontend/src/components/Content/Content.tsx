ae1178845622d133da35f6f75e08763613aab3ce
Sandun Induranga <sandutharu40@gmail.com>
2023-03-29 00:59:52 +0530

    Tested Axios

--- Frontend/frontend/src/components/Content/Content.tsx
+++ Frontend/frontend/src/components/Content/Content.tsx
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import Admin from "../../pages/Admin";
 import Main from "../../pages/Main";
+import Header from "../Header";
 
 export default class Content extends Component {
   render() {
--- Frontend/frontend/src/components/Header/Header.tsx
+++ Frontend/frontend/src/components/Header/Header.tsx
@@ -1,7 +1,20 @@
+import axios from "axios";
 import { Component } from "react";
 import logo from "../../assets/logo.png";
 
 export default class Header extends Component {
+  setRequest = () => {
+    axios({
+      method: "get",
+      url: "test",
+    })
+      .then(function (res) {
+        alert("OK");
+      })
+      .catch(function (err) {
+        alert("Error");
+      });
+  };
   render() {
     return (
       <div className="w-full h-28 flex items-center p-6 fixed top-0 bg-white">
@@ -9,7 +22,9 @@ export default class Header extends Component {
           <img src={logo} alt="" />
         </section>
         <section className="w-1/2 h-full text-xl text-green-800 flex justify-end gap-10 items-center">
-          <a href="#">Home</a>
+          <a href="#" onClick={this.setRequest}>
+            Home
+          </a>
           <a href="#">About</a>
           <a href="#">Products</a>
           <a href="#">Contact</a>
