141c57786915d155f8be375aca350a8a3bf5949c
Sandun Induranga <sandutharu40@gmail.com>
2023-03-28 21:50:21 +0530

    Entity Payment Class Updated

--- Backend/src/main/java/com/fresh/fruit_market/entity/Payment.java
+++ Backend/src/main/java/com/fresh/fruit_market/entity/Payment.java
@@ -1,9 +1,33 @@
 package com.fresh.fruit_market.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 /**
  * @author: Sandun Induranga
  * @since: 0.1.0
  **/
 
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer paymentId;
+    @ManyToOne(cascade = {CascadeType.REFRESH, CascadeType.DETACH})
+    @JoinColumn(name = "orderId", referencedColumnName = "orderId", nullable = false)
+    private Order orderId;
+    private LocalDate date;
+    private LocalTime time;
+    private BigDecimal total;
 }
