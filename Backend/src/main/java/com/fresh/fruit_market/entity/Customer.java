02d8af15f56870d183c7e39e8b5e2ce62d452305
Sandun Induranga <sandutharu40@gmail.com>
2023-03-28 21:11:36 +0530

    Entity Customer Class Created

--- Backend/src/main/java/com/fresh/fruit_market/entity/Customer.java
+++ Backend/src/main/java/com/fresh/fruit_market/entity/Customer.java
@@ -1,9 +1,27 @@
 package com.fresh.fruit_market.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class Customer {
+    @Id
     private String nic;
     private String name;
     private String license;
     private String contact;
     private String email;
+    @OneToOne(cascade = CascadeType.ALL)
+    private User user;
 }
