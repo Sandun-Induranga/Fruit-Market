d75cf1cb5e0e28522c28ee479cebd2e95e288361
Sandun Induranga <sandutharu40@gmail.com>
2023-03-28 21:27:24 +0530

    Entity Order Class Created

---
+++ Backend/src/main/java/com/fresh/fruit_market/entity/Order.java
@@ -0,0 +1,28 @@
+package com.fresh.fruit_market.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class Order {
+    @Id
+    private String orderId;
+    @ManyToOne(cascade = {CascadeType.REFRESH, CascadeType.DETACH})
+    @JoinColumn(name = "nic", referencedColumnName = "nic", nullable = false)
+    private Customer nic;
+    private LocalDate date;
+    private LocalTime time;
+    private String status;
+    private BigDecimal cost;
+}
