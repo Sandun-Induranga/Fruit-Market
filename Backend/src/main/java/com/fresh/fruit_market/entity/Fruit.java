c427fc6634eeead0176305a4cf9039f3c90ae426
Sandun Induranga <sandutharu40@gmail.com>
2023-03-28 21:20:09 +0530

    Entity Fruit Class Created

---
+++ Backend/src/main/java/com/fresh/fruit_market/entity/Fruit.java
@@ -0,0 +1,23 @@
+package com.fresh.fruit_market.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class Fruit {
+    @Id
+    private String id;
+    private String name;
+    private BigDecimal qtyOnHand;
+    private BigDecimal unitPrice;
+}
