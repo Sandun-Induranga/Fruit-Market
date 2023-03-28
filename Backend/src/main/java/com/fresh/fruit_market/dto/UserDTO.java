8051765637cf21bdfd5d43d9ffeed36b10e7127a
Sandun Induranga <sandutharu40@gmail.com>
2023-03-28 22:06:30 +0530

    UserDTO Created

---
+++ Backend/src/main/java/com/fresh/fruit_market/dto/UserDTO.java
@@ -0,0 +1,21 @@
+package com.fresh.fruit_market.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * @author : Sandun Induranga
+ * @since : 0.1.0
+ **/
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class UserDTO {
+    private String username;
+    private String password;
+    private String role;
+}
