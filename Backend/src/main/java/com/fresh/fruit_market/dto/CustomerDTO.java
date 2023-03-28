07b851a144515f224340fdc651d8ba4189d6254c
Sandun Induranga <sandutharu40@gmail.com>
2023-03-28 22:07:29 +0530

    CustomerDTO Created

--- Backend/src/main/java/com/fresh/fruit_market/dto/CustomerDTO.java
+++ Backend/src/main/java/com/fresh/fruit_market/dto/CustomerDTO.java
@@ -1,9 +1,24 @@
 package com.fresh.fruit_market.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 /**
  * @author : Sandun Induranga
  * @since : 0.1.0
  **/
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class CustomerDTO {
+    private String nic;
+    private String name;
+    private String address;
+    private String contact;
+    private String email;
+    private UserDTO user;
 }
