dd0cff901059c67a052fda05d583a13c84d00768
Sandun Induranga <sandutharu40@gmail.com>
2023-04-03 21:04:57 +0530

    Updated AppWideExceptionHandler

--- Backend/src/main/java/com/fresh/fruit_market/advisor/AppWideExceptionHandler.java
+++ Backend/src/main/java/com/fresh/fruit_market/advisor/AppWideExceptionHandler.java
@@ -1,9 +1,23 @@
 package com.fresh.fruit_market.advisor;
 
+import com.fresh.fruit_market.util.ResponseUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
 /**
  * @author : Sandun Induranga
  * @since : 0.1.0
  **/
 
+@RestControllerAdvice
+@CrossOrigin
 public class AppWideExceptionHandler {
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseUtil handleExceptions(RuntimeException e){
+        return new ResponseUtil("Error",e.getMessage(),"");
+    }
 }
