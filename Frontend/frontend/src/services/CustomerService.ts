679d340098c1df9377e6eee646466532a4ea0451
Sandun Induranga <sandutharu40@gmail.com>
2023-04-03 23:32:49 +0530

    Created Method fetchCustomer

--- Frontend/frontend/src/services/CustomerService.ts
+++ Frontend/frontend/src/services/CustomerService.ts
@@ -14,5 +14,18 @@ class CustomerService {
     });
     return await promise;
   };
+  fetchCustomer = async () => {
+    const promise = new Promise((resolve, reject) => {
+      axios
+        .post("http://localhost:8080/fresh/customer")
+        .then((res) => {
+          return resolve(res);
+        })
+        .catch((err) => {
+          return resolve(err);
+        });
+    });
+    return await promise;
+  };
 }
 export default new CustomerService();
