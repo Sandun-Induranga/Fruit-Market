392cf0a1c8f25d8094c20a3074e29641f0716b8c
Sandun Induranga <sandutharu40@gmail.com>
2023-04-03 16:00:09 +0530

    Model Mapper Added to Project

--- Backend/src/main/java/com/fresh/fruit_market/WebAppInitializer.java
+++ Backend/src/main/java/com/fresh/fruit_market/WebAppInitializer.java
@@ -1,13 +1,20 @@
 package com.fresh.fruit_market;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WebAppInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(WebAppInitializer.class);
+    }
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
     }
 
 }
--- Backend/src/main/java/com/fresh/fruit_market/service/impl/CustomerServiceImpl.java
+++ Backend/src/main/java/com/fresh/fruit_market/service/impl/CustomerServiceImpl.java
@@ -3,6 +3,7 @@ package com.fresh.fruit_market.service.impl;
 import com.fresh.fruit_market.dto.CustomerDTO;
 import com.fresh.fruit_market.repo.CustomerRepo;
 import com.fresh.fruit_market.service.CustomerService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +20,9 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Autowired
     CustomerRepo customerRepo;
+
+    @Autowired
+    ModelMapper mapper;
 
     @Override
     public void saveCustomer(CustomerDTO customerDTO) throws RuntimeException {
