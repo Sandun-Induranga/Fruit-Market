b82a6813acb5f784745fd07faa16399f675f8fc0
Sandun Induranga <sandutharu40@gmail.com>
2023-04-03 15:53:47 +0530

    CustomerServiceImpl Created

--- Backend/src/main/java/com/fresh/fruit_market/service/impl/CustomerServiceImpl.java
+++ Backend/src/main/java/com/fresh/fruit_market/service/impl/CustomerServiceImpl.java
@@ -1,14 +1,24 @@
 package com.fresh.fruit_market.service.impl;
 
 import com.fresh.fruit_market.dto.CustomerDTO;
+import com.fresh.fruit_market.repo.CustomerRepo;
 import com.fresh.fruit_market.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 /**
  * @author : Sandun Induranga
  * @since : 0.1.0
  **/
 
+@Service
+@Transactional
 public class CustomerServiceImpl implements CustomerService {
+
+    @Autowired
+    CustomerRepo customerRepo;
 
     @Override
     public void saveCustomer(CustomerDTO customerDTO) throws RuntimeException {
