d3837ff50bb2da530e184e5adad8aa95644ee4fb
Sandun Induranga <sandutharu40@gmail.com>
2023-04-03 15:51:16 +0530

    Save Customer

--- Backend/src/main/java/com/fresh/fruit_market/service/CustomerService.java
+++ Backend/src/main/java/com/fresh/fruit_market/service/CustomerService.java
@@ -1,9 +1,12 @@
 package com.fresh.fruit_market.service;
 
+import com.fresh.fruit_market.dto.CustomerDTO;
+
 /**
  * @author : Sandun Induranga
  * @since : 0.1.0
  **/
 
 public interface CustomerService {
+    public void saveCustomer(CustomerDTO customerDTO) throws RuntimeException;
 }
---
+++ Backend/src/main/java/com/fresh/fruit_market/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,17 @@
+package com.fresh.fruit_market.service.impl;
+
+import com.fresh.fruit_market.dto.CustomerDTO;
+import com.fresh.fruit_market.service.CustomerService;
+
+/**
+ * @author : Sandun Induranga
+ * @since : 0.1.0
+ **/
+
+public class CustomerServiceImpl implements CustomerService {
+
+    @Override
+    public void saveCustomer(CustomerDTO customerDTO) throws RuntimeException {
+
+    }
+}
