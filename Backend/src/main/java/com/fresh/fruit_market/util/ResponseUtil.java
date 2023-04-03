6bb34f09296aa6b49b100486d02179a3c693ad63
Sandun Induranga <sandutharu40@gmail.com>
2023-04-03 21:01:50 +0530

    Created ResponseUtil Class

---
+++ Backend/src/main/java/com/fresh/fruit_market/util/ResponseUtil.java
@@ -0,0 +1,21 @@
+package com.fresh.fruit_market.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * @author : Sandun Induranga
+ * @since : 0.1.0
+ **/
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class ResponseUtil {
+    private String state;
+    private String message;
+    private Object data;
+}
