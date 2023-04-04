1d0be17407309d8ad3257cbe5ad0583b2967090e
Sandun Induranga <sandutharu40@gmail.com>
2023-04-04 18:01:18 +0530

    Frontend Updated

--- Frontend/frontend/src/pages/Admin/Customer/Customer.tsx
+++ Frontend/frontend/src/pages/Admin/Customer/Customer.tsx
@@ -39,13 +39,14 @@ const style = {
   p: 4,
 };
 
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
+let rows: CustomerData[];
+// = [
+// createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+// createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+// createData("Eclair", 262, 16.0, 24, 6.0),
+// createData("Cupcake", 305, 3.7, 67, 4.3),
+// createData("Gingerbread", 356, 16.0, 49, 3.9),
+// ];
 
 const Customer = () => {
   let customer: CustomerData = {
@@ -67,7 +68,7 @@ const Customer = () => {
 
     console.log(res.data);
     if (res.state === 200) {
-      console.log(res.data);
+      rows = res.data;
     }
   };
 
@@ -219,21 +220,23 @@ const Customer = () => {
                   <TableCell align="right">Fat&nbsp;(g)</TableCell>
                   <TableCell align="right">Carbs&nbsp;(g)</TableCell>
                   <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                  <TableCell align="right">Protein&nbsp;(g)</TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
                 {rows.map((row) => (
                   <TableRow
-                    key={row.name}
+                    key={row.nic}
                     sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
                   >
                     <TableCell component="th" scope="row">
                       {row.name}
                     </TableCell>
-                    <TableCell align="right">{row.calories}</TableCell>
-                    <TableCell align="right">{row.fat}</TableCell>
-                    <TableCell align="right">{row.carbs}</TableCell>
-                    <TableCell align="right">{row.protein}</TableCell>
+                    <TableCell align="right">{row.address}</TableCell>
+                    <TableCell align="right">{row.contact}</TableCell>
+                    <TableCell align="right">{row.email}</TableCell>
+                    <TableCell align="right">{row.user.username}</TableCell>
+                    <TableCell align="right">{row.user.password}</TableCell>
                   </TableRow>
                 ))}
               </TableBody>
