11b1744360eeddbaae0eaba1757a4aca54e73306
Sandun Induranga <sandutharu40@gmail.com>
2023-04-20 15:36:42 +0530

    Created Method saveFruitImage()

--- Backend/src/main/java/com/fresh/fruit_market/service/ItemService.java
+++ Backend/src/main/java/com/fresh/fruit_market/service/ItemService.java
@@ -2,11 +2,13 @@ package com.fresh.fruit_market.service;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 /**
  * @author : Sandun Induranga
  * @since : 0.1.0
  **/
 
 public interface ItemService {
-    public void saveFruitImage(MultipartFile file) throws RuntimeException;
+    public void saveFruitImage(MultipartFile file) throws RuntimeException, IOException;
 }
--- Backend/src/main/java/com/fresh/fruit_market/service/impl/ItemServiceImpl.java
+++ Backend/src/main/java/com/fresh/fruit_market/service/impl/ItemServiceImpl.java
@@ -1,16 +1,27 @@
 package com.fresh.fruit_market.service.impl;
 
 import com.fresh.fruit_market.service.ItemService;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.transaction.Transactional;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * @author : Sandun Induranga
  * @since : 0.1.0
  **/
 
+@Service
+@Transactional
 public class ItemServiceImpl implements ItemService {
     @Override
     public void saveFruitImage(MultipartFile file) throws RuntimeException {
-
+        try {
+            file.transferTo(new File("http://localhost:8080/fresh" + file.getOriginalFilename()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
