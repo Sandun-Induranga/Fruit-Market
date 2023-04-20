b5986abe8ad5a71709bce2a3c9c7880043a6c722
Sandun Induranga <sandutharu40@gmail.com>
2023-04-20 20:15:43 +0530

    Created Entity Customer

---
+++ Backend/customer-service/src/main/java/lk/fresh/customerservice/entity/Customer.java
@@ -0,0 +1,28 @@
+package lk.fresh.customerservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * @author : Sandun Induranga
+ * @since : 0.1.0
+ **/
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class Customer {
+    @Id
+    private String nic;
+    private String name;
+    private String address;
+    private String contact;
+    private String email;
+}
