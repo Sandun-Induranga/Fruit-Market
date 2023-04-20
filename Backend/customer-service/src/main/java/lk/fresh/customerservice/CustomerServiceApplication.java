054b58b630f06ba7c8be1bde825247ba34681616
Sandun Induranga <sandutharu40@gmail.com>
2023-04-20 23:55:32 +0530

    Content Updated

--- Backend/customer-service/src/main/java/lk/fresh/customerservice/CustomerServiceApplication.java
+++ Backend/customer-service/src/main/java/lk/fresh/customerservice/CustomerServiceApplication.java
@@ -3,9 +3,11 @@ package lk.fresh.customerservice;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
