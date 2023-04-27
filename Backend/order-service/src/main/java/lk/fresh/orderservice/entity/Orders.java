bd956be85c87eee4c182b6392c70b6c61f51b0b5
Sandun Induranga <sandutharu40@gmail.com>
2023-04-27 16:52:01 +0530

    Order Entity Created

--- Backend/order-service/src/main/java/lk/fresh/orderservice/entity/Orders.java
+++ Backend/order-service/src/main/java/lk/fresh/orderservice/entity/Orders.java
@@ -1,10 +1,12 @@
-package lk.fresh.orderservice.dto;
+package lk.fresh.orderservice.entity;
 
+import lk.fresh.orderservice.dto.OrderDetailDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -15,11 +17,13 @@ import java.util.ArrayList;
  * @since : 0.1.0
  **/
 
+@Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @ToString
-public class OrderDTO {
+public class Orders {
+    @Id
     private String orderId;
     private String customerId;
     private LocalDate date;
