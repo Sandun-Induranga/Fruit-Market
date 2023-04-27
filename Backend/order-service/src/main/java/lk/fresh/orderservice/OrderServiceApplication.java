3ad792a3aced2877ee22801361577aa3d9ab28c4
Sandun Induranga <sandutharu40@gmail.com>
2023-04-27 16:34:35 +0530

    Order App Updated

--- Backend/order-service/src/main/java/lk/fresh/orderservice/OrderServiceApplication.java
+++ Backend/order-service/src/main/java/lk/fresh/orderservice/OrderServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.orderservice;
+package lk.fresh.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ Backend/order-service/src/main/java/lk/fresh/orderservice/advisor/AppWideExceptionHandler.java
@@ -0,0 +1,23 @@
+package lk.fresh.orderservice.advisor;
+
+import lk.fresh.orderservice.util.ResponseUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * @author : Sandun Induranga
+ * @since : 0.1.0
+ **/
+
+@RestControllerAdvice
+@CrossOrigin
+public class AppWideExceptionHandler {
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseUtil handleExceptions(RuntimeException e) {
+        return new ResponseUtil("Error", e.getMessage(), "");
+    }
+}
