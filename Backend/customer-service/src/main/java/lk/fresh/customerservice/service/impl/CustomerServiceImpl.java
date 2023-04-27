679ec14480778672818e51e3d533fd8cfc103155
Sandun Induranga <sandutharu40@gmail.com>
2023-04-27 15:44:35 +0530

    Customer App Updated

--- Backend/customer-service/src/main/java/lk/fresh/customerservice/service/impl/CustomerServiceImpl.java
+++ Backend/customer-service/src/main/java/lk/fresh/customerservice/service/impl/CustomerServiceImpl.java
@@ -47,6 +47,6 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Override
     public void deleteCustomer(String id) throws RuntimeException {
-
+        customerRepo.deleteById(id);
     }
 }
