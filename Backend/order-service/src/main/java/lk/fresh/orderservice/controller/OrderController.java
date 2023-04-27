09e972350ca9c5d41465c989e1012cfef10797f7
Sandun Induranga <sandutharu40@gmail.com>
2023-04-27 16:42:35 +0530

    Order App Updated

--- Backend/order-service/src/main/java/lk/fresh/orderservice/controller/OrderController.java
+++ Backend/order-service/src/main/java/lk/fresh/orderservice/controller/OrderController.java
@@ -1,9 +1,14 @@
 package lk.fresh.orderservice.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author : Sandun Induranga
  * @since : 0.1.0
  **/
 
+@RestController
+@RequestMapping("/api/v1/order")
 public class OrderController {
 }
---
+++ Backend/order-service/src/main/java/lk/fresh/orderservice/dot/OrderDTO.java
@@ -0,0 +1,29 @@
+package lk.fresh.orderservice.dot;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+/**
+ * @author : Sandun Induranga
+ * @since : 0.1.0
+ **/
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class OrderDTO {
+    private String orderId;
+    private String customerId;
+    private LocalDate date;
+    private LocalTime time;
+    private String location;
+    private String status;
+    private BigDecimal cost;
+}
